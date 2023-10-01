0e0fbbfae743caac8231469d2116bcd9fe53f1cf
Nante <nantemino15@gmail.com>
2023-10-01 23:25:17 +0300

    tes

--- src/main/java/com/example/restapi/controllers/CvController.java
+++ src/main/java/com/example/restapi/controllers/CvController.java
@@ -1,26 +1,20 @@
 package com.example.restapi.controllers;
 
-import java.nio.file.FileAlreadyExistsException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.restapi.customException.FileException;
 import com.example.restapi.model.Nationalite;
 import com.example.restapi.model.cv.DetailsCv;
 import com.example.restapi.services.CVService;
@@ -69,20 +63,28 @@ public class CvController {
         }
     }
 
-    @PostMapping(value = "/create", consumes = { "multipart/form-data" })
-    public ResponseEntity<Object> createCV(@RequestPart(name = "info") DetailsCv cvInfo,
-            @RequestPart(value = "cv") MultipartFile cvFile, @RequestPart MultipartFile certificat) {
-        System.out.println(">>>>>>>>>>>>>>>> " + cvInfo.toString());
-        // try {
-        // this.cvService.save(cvInfo, cvFile, certificat);
-        // return ResponseEntity.status(200).body(new HashMap<String,
-        // Object>().put("msg", "CV créé"));
-        // } catch (FileAlreadyExistsException | FileException e) {
-        // e.printStackTrace();
-        // return ResponseEntity.status(500).body(new HashMap<String,
-        // Object>().put("msg", e.getMessage()));
-        // }
-        return ResponseEntity.ok().body("ok");
+    // @PostMapping(value = "/create", consumes = { "multipart/form-data" })
+    // public ResponseEntity<Object> createCV(@RequestPart(name = "info") DetailsCv
+    // cvInfo,
+    // @RequestPart(value = "cv") MultipartFile cvFile, @RequestPart MultipartFile
+    // certificat) {
+    // System.out.println(">>>>>>>>>>>>>>>> " + cvInfo.toString());
+    // try {
+    // this.cvService.save(cvInfo, cvFile, certificat);
+    // return ResponseEntity.status(200).body(new HashMap<String,
+    // Object>().put("msg", "CV créé"));
+    // } catch (FileAlreadyExistsException | FileException e) {
+    // e.printStackTrace();
+    // return ResponseEntity.status(500).body(new HashMap<String,
+    // Object>().put("msg", e.getMessage()));
+    // }
+    // return ResponseEntity.ok().body("ok");
+    // }
+
+    @PostMapping(value = "/create", consumes = "multipart/form-data")
+    public DetailsCv createCV(
+            @RequestParam(name = "cvinfo") DetailsCv cv) {
+        return cv;
     }
 
     @PostMapping(value = "test")
